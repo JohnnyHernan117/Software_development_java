70f89391caf95c1ada97a4023e1b47e9bde3f2da
Johnny Hernandez <johnnyhernan117@gmail.com>
2024-11-01 19:58:33 -0700

    Add files via upload

---
+++ overloadedmethods/OverloadedMethods.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.overloadedmethods;
+/**
+ *
+ * @author johnn
+ */
+public class OverloadedMethods {
+
+    public static <T> void printArray(T[] inputArray) {
+        for (T element : inputArray) 
+            System.out.printf("%s ", element);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // create arrays of Integer, Double and Character
+        Integer[] integerArray = {1, 2, 3, 4, 5, 6};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+        Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
+        System.out.printf("Array integerArray contains: ");
+        printArray(integerArray); //pass an Integer array
+        System.out.printf("Array doubleArray contains: ");
+        printArray(doubleArray); //pass a Double array
+        System.out.printf("Array characterArray contains: ");
+        printArray(characterArray); // pass a Character array
+    }
+}
